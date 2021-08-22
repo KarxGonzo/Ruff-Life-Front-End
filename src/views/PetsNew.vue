17a0f7d831d5f1629ef348795377f06736fc34ef
Karina Gonzalez <KarxGonzo@gmail.com>
2021-08-22 10:32:03 -0400

    Pets New - Formatting and Styling.

--- src/views/PetsNew.vue
+++ src/views/PetsNew.vue
@@ -1,33 +1,57 @@
 <template>
-  <div class="pets-new">
+  <section class="Pet-New">
     <form v-on:submit.prevent="submit()">
-      <section class="py-5">
-        <div class="container px-5">
-          <h1 class="fw-bolder fs-5 mb-4">Create a New Pet</h1>
-              <div class="card border-0 shadow rounded-3 overflow-hidden">
-                <div class="card-body p-0">
-                  <div class="row gx-0">
-                    <div class="col-sm-0 col-sm-0 py-sm-0">
-                      <div class="p-4 p-md-5">
-                         <div>
-                            <label>Name:</label>
-                            <input type="text" v-model="newPetParams.name" />
-                          </div>
-                          <div>
-                            <label>Picture:</label>
-                            <input type="text" v-model="newPetParams.picture" />
-                          </div>
-                          <input type="submit" value="Submit" />
+      <ul> <li v-for="error in errors" v-bind:key="error">{{ error }}</li></ul>
+                <div class="container px-5">
+                    <div class="bg-light rounded-3 py-5 px-4 px-md-5 mb-5">
+                        <div class="text-center mb-5">
+                            <p class="lead fw-normal text-muted mb-0">Create a New Pet</p>
                         </div>
-                      </div>
+                        <div class="row gx-5 justify-content-center">
+                            <div class="col-lg-8 col-xl-6">
+
+                                    <!-- Name input-->
+                                    <div class="form-floating mb-3">
+                                        <input 
+                                        class="form-control" 
+                                        id="name" 
+                                        type="text" 
+                                        v-model="newPetParams.name" 
+                                        placeholder="Enter the name of your pet..." data-sb-validations="required" />
+                                        <label for="name">Name</label>
+                                        <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
+                                    </div>
+                                    <!-- Picture Input-->
+                                    <div class="form-floating mb-3">
+                                        <input 
+                                        class="form-control" 
+                                        id="picture" 
+                                        type="text"  
+                                        placeholder="Picture URL"
+                                        v-model="newPetParams.picture" />
+                                        <label for="Picture">Picture URL</label>
+                                    </div>
+                                    <!-- Submit success message-->
+                                    <!---->
+                                    <!-- This is what your users will see when the form-->
+                                    <!-- has successfully submitted-->
+                                    <div class="d-none" id="submitSuccessMessage">
+                                        <div class="text-center mb-3">
+                                            <div class="fw-bolder">Form submission successful!</div>
+                                            To activate this form, sign up at
+                                            <br />
+                                            <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                                        </div>
+                                    </div>
+                                    <!-- Submit Button-->
+                                    <div class="d-grid"><button class="btn btn-primary btn-lg" id="submitButton" type="submit">Submit</button></div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
+                    </div>
                 </div>
-          </div>
-          <br>
-      </section>
     </form>
-  </div>
+  </section>
 </template>
 
 <script>
--- src/views/PetsShow.vue
+++ src/views/PetsShow.vue
@@ -5,17 +5,18 @@
                     <div class="row gx-5 justify-content-center">
                         <div class="col-lg-8 col-xl-6">
                             <div class="text-center">
-                                <p class="lead fw-normal text-muted mb-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque fugit ratione dicta mollitia. Officiis ad.</p>
+                               <img class="card-img-top" img v-bind:src="pet.picture" />
+                                <p class="lead fw-normal text-muted mb-5">Name: {{ pet.name }}</p>
                             </div>
                         </div>
                     </div>
                     <div class="row gx-5">
                         <div class="col-lg-4 mb-5">
                             <div class="card h-100 shadow border-0">
-                                <img class="card-img-top" src="https://dummyimage.com/600x350/ced4da/6c757d" alt="..." />
+                                <img class="card-img-top" img v-bind:src="pet.picture" />
                                 <div class="card-body p-4">
-                                    <a class="text-decoration-none link-dark stretched-link" href="#!"><h5 class="card-title mb-3">Blog post title</h5></a>
-                                    <p class="card-text mb-0">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a class="text-decoration-none link-dark stretched-link" href="#!"><h5 class="card-title mb-3">Favorite Snack: {{ pet.favorite_snacks[0][1]}}</h5></a>
+                                    <p class="card-text mb-0">Favorite Snacks: {{ pet.favorite_snacks }}</p>
                                 </div>
                             </div>
                         </div>
